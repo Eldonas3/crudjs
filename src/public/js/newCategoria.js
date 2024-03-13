25d9cc24de416e041ea11463b828a5e748bc654d
MariaGuadalupe08 <lupita080209@gmail.com>
2024-03-12 22:04:19 -0600

    vista de agregar nueva catego

---
+++ src/public/js/newCategoria.js
@@ -0,0 +1,41 @@
+const btn = document.getElementById('btn');
+
+$(document).ready(function() {
+    $("#alert").hide();
+});
+
+$("#btn-alert").click(function() {
+    $("#alert").hide();
+});
+
+btn.onclick = () => {
+    const nombre = document.getElementById('nombre').value;
+    const descripcion = document.getElementById('descripcion').value;
+
+    if (nombre == '' || descripcion == '') {
+        $("#alert").show();
+    } else {
+
+        //guardamos datos en la base de datos 
+        postData(nombre, descripcion);
+    }
+
+}
+
+async function postData(nombre, descripcion) {
+    const response = await fetch('/api/save', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'nombre': nombre,
+            'descripcion': descripcion
+        })
+
+    });
+
+    const data = await response.json();
+    console.log(data);
+}
